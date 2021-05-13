f83ea7b3b3edf5149d5abab60fd30c485d078aa4
SkaillZ <skaillzsupp@gmail.com>
2021-05-13 10:30:08 +0200

    Add pre-commit hook and CI build

--- Main.cpp
+++ Main.cpp
@@ -6,23 +6,23 @@
 #define STB_LEAKCHECK_IMPLEMENTATION
 #include <stb_leakcheck.h>
 
-int main(int argc, char** argv) {  
+int main(int argc, char **argv) {
     /*
-    * Initialises the SDL video subsystem (as well as the events subsystem).
-    * Returns 0 on success or a negative error code on failure using SDL_GetError().
-    */
+     * Initialises the SDL video subsystem (as well as the events subsystem).
+     * Returns 0 on success or a negative error code on failure using SDL_GetError().
+     */
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
         return 1;
     }
 
     /* Creates a SDL window */
-    auto window = SDL_CreateWindow("SDL Example", /* Title of the SDL window */
-                    SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
-                    SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
-                    1280, /* Width of the window in pixels */
-                    720, /* Height of the window in pixels */
-                    0); /* Additional flag(s) */
+    auto window = SDL_CreateWindow("SDL Example",           /* Title of the SDL window */
+                                   SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
+                                   SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+                                   1280,                    /* Width of the window in pixels */
+                                   720,                     /* Height of the window in pixels */
+                                   0);                      /* Additional flag(s) */
 
     /* Checks if window has been created; if not, exits program */
     if (!window) {
@@ -36,57 +36,52 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    //Initialize PNG loading
+    // Initialize PNG loading
     int imgFlags = IMG_INIT_PNG;
-    if (!(IMG_Init(imgFlags) & imgFlags))
-    {
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
         printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
         return 1;
     }
 
     auto loadedSurface = IMG_Load("image.png");
-    SDL_Surface* pngImage;
-    if (loadedSurface == nullptr)
-    {
+    SDL_Surface *pngImage;
+    if (loadedSurface == nullptr) {
         printf("Unable to load image! SDL_image Error: %s\n", IMG_GetError());
         return 1;
-    }
-    else
-    {
-        //Convert surface to screen format
+    } else {
+        // Convert surface to screen format
         pngImage = SDL_ConvertSurface(loadedSurface, surface->format, 0);
-        if (pngImage == nullptr)
-        {
+        if (pngImage == nullptr) {
             printf("Unable to optimize image! SDL Error: %s\n", SDL_GetError());
             return 1;
         }
 
-        //Get rid of old loaded surface
+        // Get rid of old loaded surface
         SDL_FreeSurface(loadedSurface);
     }
 
     SDL_BlitSurface(pngImage, nullptr, surface, nullptr);
     SDL_UpdateWindowSurface(window);
 
-    bool quit = false;                                      
-    SDL_Event e;                                            
-    while (!quit) {                                         
-        while (SDL_PollEvent(&e)) {                         
-            if (e.type == SDL_QUIT) {                       
-                quit = true;                                
-            }                                               
-        }                                                   
-    }     
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
 
     /* Frees memory */
     SDL_DestroyWindow(window);
     SDL_FreeSurface(pngImage);
-    
-    /* Shuts down all SDL subsystems */
-    SDL_Quit(); 
 
-    void* memoryLeak = malloc(10);
+    /* Shuts down all SDL subsystems */
+    SDL_Quit();
+
+    void *memoryLeak = malloc(10);
     stb_leakcheck_dumpmem();
-    
+
     return 0;
 }
---
+++ tools/install-hooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clang_format_command="clang-format"
+
+cat <<EOF > .git/hooks/pre-commit
+#!/bin/sh
+FILES=\$(git diff --cached --name-only --diff-filter=ACMR "*.h" "*.cpp" | sed 's| |\\ |g')
+[ -z "\$FILES" ] && exit 0
+echo "Applying auto formatting..."
+# Format all changed files
+echo "\$FILES" | xargs $clang_format_command -i -style=file
+# Add back the modified files to staging
+echo "\$FILES" | xargs git add
+EOF
+
+chmod +x .git/hooks/pre-commit
+
+echo "Installed pre-commit hook."

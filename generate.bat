5eeffd0e94e1c630ff15c69b5c303baa493d6887
Annabelle Nissl <annabelle.nissl@gmail.com>
2021-05-20 16:47:22 +0200

    removed W3 sad

--- generate.bat
+++ generate.bat
@@ -1,5 +1,11 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
+if exist build\ (
+  meson build --wipe --backend vs
+) else (
+  meson build --backend vs
+)
+
 meson compile -C build -j0
 
 build\complementary.exe

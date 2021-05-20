5d0183c595993393a8559453fed4adb5fe141cc5
Annabelle Nissl <annabelle.nissl@gmail.com>
2021-05-20 16:04:19 +0200

    edited bat file

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 meson build --backend vs
 meson compile -C build -j0
+
+build\complementary.exe

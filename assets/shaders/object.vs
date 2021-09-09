87e8b570894f0fc57290717d068be00f0c0ff195
Kajetan Johannes Hammerle <kajetan.johannes.hammerle@gmail.com>
2021-09-09 15:35:29 +0200

    fix mac pipeline

--- assets/shaders/object.vs
+++ assets/shaders/object.vs
@@ -4,7 +4,7 @@ layout(location = 0) in vec2 pos;
 layout(location = 1) in vec4 color;
 
 uniform mat4 view;
-uniform float zLayer; 
+uniform float zLayer;
 out vec4 varColor;
 
 void main() {
--- src/tilemap/TilemapEditor.cpp
+++ src/tilemap/TilemapEditor.cpp
@@ -26,7 +26,9 @@ static int tilemapBackgroundColor;
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-variable"
+#ifdef __linux__
 #pragma GCC diagnostic ignored "-Waggressive-loop-optimizations"
+#endif
 #define STB_TILEMAP_EDITOR_IMPLEMENTATION
 #include <SDL.h>
 #define _SDL_H

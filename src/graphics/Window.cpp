9a2fd552272200a06f779dfa7adc1f79f437e6dc
Annabelle Nissl <annabelle.nissl@gmail.com>
2021-10-14 14:32:57 +0200

    add combined world switch and ability key

--- src/graphics/Window.cpp
+++ src/graphics/Window.cpp
@@ -126,8 +126,8 @@ static void pollEvents() {
                             Input::Internal::setButtonPressed(ButtonType::SWITCH);
                             break;
                         }
+                        case SDLK_RSHIFT: Input::Internal::setButtonPressed(ButtonType::SWITCH);
                         case SDLK_LSHIFT:
-                        case SDLK_RSHIFT:
                             Input::Internal::setButtonPressed(ButtonType::ABILITY);
                             break;
                         case SDLK_LEFT:
@@ -143,8 +143,8 @@ static void pollEvents() {
                 switch (e.key.keysym.sym) {
                     case SDLK_SPACE: Input::Internal::setButtonReleased(ButtonType::JUMP); break;
                     case SDLK_RETURN: Input::Internal::setButtonReleased(ButtonType::SWITCH); break;
+                    case SDLK_RSHIFT: Input::Internal::setButtonReleased(ButtonType::SWITCH);
                     case SDLK_LSHIFT:
-                    case SDLK_RSHIFT:
                         Input::Internal::setButtonReleased(ButtonType::ABILITY);
                         break;
                     case SDLK_LEFT:
